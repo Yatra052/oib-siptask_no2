057872c6877d6a717cfc6f611227cb4e64f00780
lenovo <vishwayatra919@gmail.com>
2023-02-07 19:59:38 +0530

    finally

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,48 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Name the National game of the USA?",
+            "Which animal is known as the 'Ship of the Desert?",
+            "Name the National Reptile of India?",
+            "Which is the principal source of energy for the Earth",
+            "Who invented Radio?",
+            "Who invented electricity?",
+            "Who gave the theory of Relativity?",
+            "Who is the real founder of Microsoft?",
+            "Name the largest 'Democracy' of the world?",
+            "Name the hardest substance available on Earth?"
+
+    };
+
+    public static String choices[][] = {
+
+            {"Cricket", "BaseBall", "Badminton", "Tennis"},
+            {"Camel","Rabbit", "Dog", "Mammals"},
+            {"Python", "KingCobra", "Snake", "Balcat"},
+            {"Moon", "Star" ,"Jupiter", "Sun"},
+            {"Röntgen","Albert Einstein","Enrico Fermi","Guglielmo Marconi "},
+            {" Benjamin Franklin", "Albert Einstein", "Michael Faraday", "Faraday"},
+            {"Erastered", "Erecth Kvian", "whilsome Blitch", " Albert Einstein"},
+            {" Bill Gates and Paul G. Allen", "Rabindra Singh", "Negis Seizman", "ferich"},
+            {"Pakistan", "China", "Russia", "India"},
+            {"Coal", "Diamond", "Platinium", "Mercury"}
+
+    };
+
+    public static String correctAnswers[] = {
+            "BaseBall",
+            "Camel",
+            "KingCobra",
+            "Sun",
+            "Röntgen", "Benjamin Franklin"," Albert Einstein"," Bill Gates and Paul G. Allen","India",
+            "Diamond"
+
+    };
+
+}
+
+
+
+
